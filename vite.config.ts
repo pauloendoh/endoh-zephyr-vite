8722c62317bad64e1e9b6cac4be31bd789266646
Paulo Endoh <paulo.endoh@gmail.com>
2025-11-29 15:02:51 -0300

    fix: fixes zephyr plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
-
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { withZephyr } from "vite-plugin-zephyr"; // Add this line
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  withZephyr()
-
-})
+  plugins: [react(), withZephyr()],
+});
